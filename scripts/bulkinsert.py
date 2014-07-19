63ec09ac586fafd78509df117e3a3f107bf7697f
조일환 <carpenta@daumcorp.com>
2014-07-19 14:13:08 +0900

    사람 검색 기능 추가

--- scripts/bulkinsert.py
+++ scripts/bulkinsert.py
@@ -9,77 +9,79 @@ import datetime
 import json
 
 class InitTool:
-	def __init__(self, filepath):
-		self.filepath = filepath
-		self.host = 'goodworker-jj.appspot.com' 
-		self.conn = httplib.HTTPConnection(self.host)
-		self.template = '{"name":"%s", "address":"%s", "group":"%s", "latitude":"%s", "longitude":"%s"}'
+  def __init__(self, filepath, host='goodworker-jj.appspot.com'):
+    self.filepath = filepath
+    self.host = host
+    self.conn = httplib.HTTPConnection(self.host)
+    self.template = '{"name":"%s", "address":"%s", "group":"%s", "latitude":"%s", "longitude":"%s"}'
 
-	def close(self):
-		try:
-			self.conn.close()
-		except:
-			traceback.print_exc()
+  def close(self):
+    try:
+      self.conn.close()
+    except:
+      traceback.print_exc()
 
-	def req(self, url='/', method='GET', data='', headers={}):
-		result = None
-		try:
-			self.conn.request(method, url, data, headers)
-			response = self.conn.getresponse()
-			result = response.read()
-		except:
-			traceback.print_exc()
-		return result
+  def req(self, url='/', method='GET', data='', headers={}):
+    result = None
+    try:
+      self.conn.request(method, url, data, headers)
+      response = self.conn.getresponse()
+      result = response.read()
+    except:
+      traceback.print_exc()
+    return result
 
-	def insert(self):
-		success_cnt = 0
-		f = open(self.filepath, 'r')
-		for l in f.xreadlines():
-			tokens = l.split('\t')
-			name = tokens[0]
-			addr = ''
-			group = ''
-			lat = ''
-			lng = ''
+  def insert(self):
+    success_cnt = 0
+    f = open(self.filepath, 'r')
+    for l in f.xreadlines():
+      tokens = l.split('\t')
+      name = tokens[0]
+      addr = ''
+      group = ''
+      lat = ''
+      lng = ''
 
-			if len(tokens) > 1:
-				addr = tokens[1].strip()
-			if len(tokens) > 2:
-				group = tokens[2].strip()
-			if len(tokens) > 4:
-				lat = tokens[3].strip()
-				lng = tokens[4].strip()
+      if len(tokens) > 1:
+        addr = tokens[1].strip()
+      if len(tokens) > 2:
+        group = tokens[2].strip()
+      if len(tokens) > 4:
+        lat = tokens[3].strip()
+        lng = tokens[4].strip()
 
-			body = self.template % (name, addr, group, lat, lng)
-			print body
-			response = self.req('/people', 'POST', body)
-			try:
-				result = json.loads(response)
-				if result['success']:
-					success_cnt +=1 
-			except:
-				print tokens
-				traceback.print_exc()
-		f.close()
-		return success_cnt
+      body = self.template % (name, addr, group, lat, lng)
+      print body
+      response = self.req('/people', 'POST', body)
+      try:
+        print response
+        result = json.loads(response)
+        if result['success']:
+          success_cnt +=1 
+      except:
+        print tokens
+        traceback.print_exc()
+    f.close()
+    return success_cnt
 
 
 if __name__ == '__main__':
-	import sys
-	reload(sys)
-	sys.setdefaultencoding('utf-8')
+  import sys
+  reload(sys)
+  sys.setdefaultencoding('utf-8')
 
-	import optparse
-	argv = sys.argv
-	argc = len(argv)
+  import optparse
+  argv = sys.argv
+  argc = len(argv)
 
-	parser = optparse.OptionParser()
-	parser.add_option('-f', '--file', default=None, help='input file')
-	(options, args) = parser.parse_args(args=argv)
+  parser = optparse.OptionParser()
+  parser.add_option('-f', '--file', default=None, help='input file')
+  parser.add_option('-t', '--host', default='goodworker-jj.appspot.com', help='input host path')
+  (options, args) = parser.parse_args(args=argv)
 
-	if options.file is not None:
-		it = InitTool(options.file)
-		it.insert()
-		it.close()
-	
-	print 'complete!'
+  if options.file is not None:
+    it = InitTool(options.file, options.host)
+    it.insert()
+    it.close()
+  
+  print 'complete!'
--- static/js/map.js
+++ static/js/map.js
@@ -1,34 +1,34 @@
 var lat = 33.4780117808;
 var lng = 126.489289086;
 function addMarker(map, position, text) {
-	var marker = new daum.maps.Marker({
-		position: position
-	});
-	marker.setTitle(text);
-	marker.setMap(map);
-	if (text != null && text.length > 0) {
-		var infowindow = new daum.maps.InfoWindow({
-			content: text,
-			disableAutoPan: true
-		});
-		infowindow.open(map, marker);
-	}
-	return marker;
+  var marker = new daum.maps.Marker({
+    position: position
+  });
+  marker.setTitle(text);
+  marker.setMap(map);
+  if (text != null && text.length > 0) {
+    var infowindow = new daum.maps.InfoWindow({
+      content: text,
+      disableAutoPan: true
+    });
+    infowindow.open(map, marker);
+  }
+  return marker;
 }
 
 function initMap(options) {
-	var position = new daum.maps.LatLng(lat, lng);
-	var map = new daum.maps.Map(document.getElementById('map'), {
-		center: position,
-		level: 4,
-		mapTypeId: daum.maps.MapTypeId.ROADMAP,
-		draggable: true
-	});
-	var zoom = new daum.maps.ZoomControl();
-	map.addControl(zoom);
+  var position = new daum.maps.LatLng(lat, lng);
+  var map = new daum.maps.Map(document.getElementById('map'), {
+    center: position,
+    level: 4,
+    mapTypeId: daum.maps.MapTypeId.ROADMAP,
+    draggable: true
+  });
+  var zoom = new daum.maps.ZoomControl();
+  map.addControl(zoom);
 
-	if (!options || !options.disableDefaultMarker) {
-		addMarker(map, position, "<div class='info-content'>충신교회</div>");
-	}
-	return map;
+  if (!options || !options.disableDefaultMarker) {
+    addMarker(map, position, "<div class='info-content'>충신교회</div>");
+  }
+  return map;
 }
